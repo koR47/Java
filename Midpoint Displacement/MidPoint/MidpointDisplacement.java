011e157312cfb6a6da18484d4010b8861df3a1a5
koR47 <wesslingviktor@gmail.com>
2016-01-10 00:33:06 +0100

    lol

--- Midpoint Displacement/MidPoint/MidpointDisplacement.java
+++ Midpoint Displacement/MidPoint/MidpointDisplacement.java
@@ -1,14 +1,14 @@
 package MidPoint;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Random;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 
 public class MidpointDisplacement extends BorderPane {
 	static Random r = new Random();
@@ -16,43 +16,41 @@ public class MidpointDisplacement extends BorderPane {
 	double [] pointR = new double[2];
 	double samples = 3;
 	double depth = 5;
-	ArrayList<Double> midpointsX = new ArrayList<Double>();
-	ArrayList<Double> midpointsY = new ArrayList<Double>();
-	
 	
 	public MidpointDisplacement (){
 		Canvas leinwand = new Canvas(600,600);
 		Label segText = new Label("Segmente");
 		Label smoothness = new Label("Smoothness");
+		TextField info = new TextField("Hi.");
 		GraphicsContext gc = leinwand.getGraphicsContext2D();
+		gc.setFill(Color.RED);
 		gc.strokeRect(0, 0, leinwand.getWidth(), leinwand.getHeight());
 		
 		Button button = new Button("New");
 		button.setMaxSize(Double.MAX_VALUE,Double.MAX_VALUE);
 		button.setOnAction( e -> {
 			gc.clearRect(0, 0, leinwand.getWidth(), leinwand.getHeight());
-			midpointsX.clear();
-			midpointsY.clear();
 			gc.strokeRect(0, 0, leinwand.getWidth(), leinwand.getHeight());
+			info.setText("Segments:  " + samples + "   ///   Smoothness:  " + depth);
 			berechne(gc, leinwand);
 		});
 		
-		Slider segmente = new Slider(1, 50, samples);
+		Slider segmente = new Slider(1, 30, samples);
 		segmente.setBlockIncrement(1);
 		segmente.valueProperty().addListener((observable, oldValue, newValue) -> {
-		    System.out.println("Neue Anzahl der Segmente: " + newValue.intValue());
 		    samples = newValue.intValue();
+		    info.setText("Segments:  " + samples + "   ///   Smoothness:  " + depth);
 		});
 		
-		Slider tiefe = new Slider(2, 10, samples);
+		Slider tiefe = new Slider(2, 8, samples);
 		tiefe.setBlockIncrement(0.1);
 		tiefe.valueProperty().addListener((observable, oldValue, newValue) -> {
-		    System.out.println("Neue Tiefe: " + newValue.doubleValue());
-		    depth = newValue.intValue();
+		    depth = newValue.doubleValue();
+		    info.setText("Segments:  " + samples + "   ///   Smoothness:  " + depth);
 		});
 		
 		VBox steuerung = new VBox();
-		steuerung.getChildren().addAll(segText, segmente, smoothness, tiefe, button);
+		steuerung.getChildren().addAll(button, segText, segmente, smoothness, tiefe, info);
 		steuerung.setSpacing(10);
 		
 		this.setBottom(steuerung);
@@ -89,29 +87,15 @@ public class MidpointDisplacement extends BorderPane {
 		
 		double midX = (xR + xL) / 2;
 		double midY = (yR + yL) / 2 + rnd;
-		midpointsX.add(midX);
-		midpointsY.add(midY);
 
 		gc.fillRect(midX, midY, 3, 3);
-		
-		Collections.sort(midpointsX);
-		System.out.println("Arraylaenge: " + midpointsX.size());
-		for (int i = 0; i < midpointsX.size() - 1; i++){
-			System.out.println("midX: " + midpointsX.get(i) + "\t | midY: " + midpointsY.get(i));
-		}
-		
-		if (midpointsX.size() == samples * 2 + 1){
-			for (int i = 0; i <= midpointsX.size() - 2; i++){
-				gc.strokeLine(midpointsX.get(i), midpointsY.get(i), midpointsX.get(i+1), midpointsY.get(i+1));
-				gc.strokeLine(midpointsX.get(i), midpointsY.get(i), midpointsX.get(i+1), midpointsY.get(i+1));
-			}
-		}
 		
 		if (xDiff > canvas.getWidth() / samples){
 			midpoint(xL, yL, midX, midY, gc, canvas);
 			midpoint(midX, midY, xR, yR, gc, canvas);
+		}else{
+			gc.strokeLine(xL, yL, midX, midY);
+			gc.strokeLine(midX, midY, xR, yR);
 		}
 	}
-	
-	
 }
